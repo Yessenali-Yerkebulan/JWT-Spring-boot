2e14987d19d2e67f1e83b5988b485614c5587973
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-03 10:54:04 +0600

    first commit

---
+++ src/main/java/com/example/security/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.security.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("HEllo from secured endpoint");
+    }
+}
---
+++ src/main/java/com/example/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
